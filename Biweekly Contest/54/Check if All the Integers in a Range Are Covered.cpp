ce3f48c2861b767e319a54d7b428fb942828f434
Mohit Varma <varmamohit122@gmail.com>
2021-06-24 15:50:54 +0530

    Create Check if All the Integers in a Range Are Covered.cpp

---
+++ Biweekly Contest/54/Check if All the Integers in a Range Are Covered.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isCovered(vector<vector<int>>& ranges, int left, int right)
+    {
+        vector<int> a(101, 0);
+
+        for (int i = 0; i < (int)size(ranges); i++) {
+            int l = ranges[i][0], r = ranges[i][1];
+
+            for (int j = l; j <= r; j++) a[j] = 1;
+        }
+
+        for (int i = left; i <= right; i++) {
+            if (a[i] == 0) return false;
+        }
+
+        return true;
+    }
+};
