37d59227603512547af5ee2c36aee96f08244c8c
Mohit Varma <70433964+captainxop@users.noreply.github.com>
2021-06-25 18:10:04 +0530

    Create Delete Operation for Two Strings.cpp

---
+++ Dynamic Programming/Delete Operation for Two Strings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) 
+    {
+        int n = word1.length(), m = word2.length(), dp[n+1][m+1];
+        
+        for(int i = 0; i <= n; i++) {
+            for(int j = 0; j <= m; j++) {
+                if(i == 0 || j == 0) dp[i][j] = 0;
+                else if(word1[i-1] == word2[j-1]) {
+                    dp[i][j] = 1 + dp[i-1][j-1]; 
+                }
+                else {
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        
+        int lcs = dp[n][m];
+        
+        return ((n - lcs) + (m - lcs));
+    }
+};
