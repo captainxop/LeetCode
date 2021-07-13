7c735a7391accbdd7b99b503fca4c2e17cab172d
Mohit Varma <70433964+captainxop@users.noreply.github.com>
2021-07-13 16:39:19 +0530

    Create Find Peak Element.cpp

---
+++ Binary Search/Find Peak Element.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) 
+    {
+        int low = 0, high = size(nums) - 1, mid = 0;
+        
+        while(low < high) {
+            mid = (low + high) / 2;
+            
+            if(nums[mid] < nums[mid + 1]) {
+                low = mid + 1;
+            }
+            else {
+                high = mid;
+            }
+        }
+        
+        return low;
+    }
+};
