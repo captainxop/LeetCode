e9922bab31665fe047134091c788fff12c50fdc4
Mohit Varma <70433964+n0IQ@users.noreply.github.com>
2022-09-03 13:40:14 +0530

    Time: 7 ms (63.59%), Space: 6.1 MB (81.81%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -0,0 +1,51 @@
+class Solution {
+private:
+    int ans;
+    
+    void totalNQueensHelper(vector<string> &board, int row, int n) {
+        if(row == n) {
+            ans++;
+            return;
+        }
+        
+        for(int col = 0; col < n; col++) {
+            if(isPossible(board, row, col, n)) {
+                board[row][col] = 'Q';
+                totalNQueensHelper(board, row + 1, n);
+                board[row][col] = '.';
+            }
+        }
+    }
+    
+    bool isPossible(vector<string> &board, int row, int col, int n) {
+        int r = row, c = col;
+        
+        while(row >= 0) { // check above
+            if(board[row][col] == 'Q') return false;
+            row--;
+        }
+        
+        row = r;
+        while(row >= 0 && col >= 0) { // check upper left diagonal
+            if(board[row][col] == 'Q') return false;
+            row--, col--;
+        }
+        
+        row = r, col = c;
+        while(row >= 0 && col < n) { // check upper right diagonal
+            if(board[row][col] == 'Q') return false;
+            row--, col++;
+        }
+        
+        return true;
+    }
+    
+public:
+    int totalNQueens(int n) {
+        ans = 0;
+        vector<string> board;
+        for(int i = 0; i < n; i++) board.push_back(string(n, '.'));
+        totalNQueensHelper(board, 0, n);
+        return ans;
+    }
+};
