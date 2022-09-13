4a053cddbe72f62f6d84b2113cbdd61dc1c8c2a7
Mohit Varma <70433964+n0IQ@users.noreply.github.com>
2022-09-13 15:41:07 +0530

    Time: 219 ms (50.85%), Space: 79.8 MB (32.84%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -1,45 +1,27 @@
 class Solution {
 private:
     vector<vector<string>> ans;
-    set<vector<string>> set;
+    vector<string> list;
     
-    void partitionHelper(string s, vector<bool> &cut, int i, int n) {
-        if(i == n) {
-            checkPartitions(s, cut, n);
+    void partitionHelper(string s, int idx, int n) {
+        if(idx == n) {
+            ans.push_back(list);
             return;
         }
         
-        partitionHelper(s, cut, i + 1, n);
-                
-        cut[i] = 1;
-        partitionHelper(s, cut, i + 1, n);
-        cut[i] = 0;
-    }
-    
-    void checkPartitions(string &s, vector<bool> &cut, int n) {
-        vector<string> temp;
-        string str;
-        
-        for(int i = 0; i < n; i++) {
-            if(cut[i] || i == n - 1) {
-                str.push_back(s[i]);
-                if(isPalindrome(str)) temp.push_back(str);
-                else return;
-                str = "";
+        for(int i = idx; i < n; i++) {
+            if(isPalindrome(s, idx, i)) {
+                list.push_back(s.substr(idx, i - idx + 1));
+                partitionHelper(s, i + 1, n);
+                list.pop_back();
             }
-            else str.push_back(s[i]);
-        }
-        
-        if(!set.count(temp)) {
-            ans.push_back(temp);
-            set.insert(temp);
         }
     }
     
-    bool isPalindrome(string &s) {
-        int n = (int)s.size();
-        for(int i = 0; i < n / 2; i++) {
-            if(s[i] != s[n - i - 1]) return false;
+    bool isPalindrome(string &s, int start, int end) {
+        while(start <= end) {
+            if(s[start] != s[end]) return false;
+            start++, end--;
         }
         return true;
     }
@@ -47,8 +29,7 @@ private:
 public:
     vector<vector<string>> partition(string s) {
         int n = (int)s.size();
-        vector<bool> cut(n, 0);
-        partitionHelper(s, cut, 0, n);
+        partitionHelper(s, 0, n);
         return ans;
     }
 };
